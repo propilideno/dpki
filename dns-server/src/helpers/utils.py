e123a842328d333f077500c968b9a4eef1a03c7f
Filipe <filipejvmf@gmail.com>
2024-08-12 21:54:09 -0300

    feat: remove flask server

--- dns-server/src/helpers/utils.py
+++
@@ -1,9 +0,0 @@
-from typing import Dict
-
-def str2bool(string: str) -> bool:
-  return string.lower() in ('yes', 'true', '1', 'y', 't')
-
-def error_dict(message: str) -> Dict[str, str]:
-  return {
-    'error': message,
-  }
